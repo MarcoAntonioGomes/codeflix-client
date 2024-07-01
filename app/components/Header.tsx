0f9d1df507d6e9cbbe14ff953e47460b4f6ac743
marco <marco@joinder.me>
2024-07-01 09:04:39 -0300

    tests config

---
+++ app/components/Header.tsx
@@ -0,0 +1,24 @@
+'use client';
+import React from 'react';
+import { UserProfile } from './UserProfile';
+import { NavLinks } from './NavLinks';
+import { Logo } from './Logo';
+import { useScroll } from '../hooks/useScroll';
+
+export default function Header() {
+  const isScrolled = useScroll();
+
+  return (
+    <header
+      className={`${
+        isScrolled && 'bg-black'
+      } fixed top-0 z-50 flex w-full items-center justify-between p-2 transition-all lg:px-10 lg:py-4`}
+    >
+      <div className='flex items-center space-x-2 md:space-x-8'>
+        <Logo />
+        <NavLinks />
+      </div>
+      <UserProfile />
+    </header>
+  );
+}
